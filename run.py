59c5103764a963ec3545494a09dcc1902d68efa6
kele.xxj” <wb-xxj538870@alibaba-inc.com>
2021-01-07 19:19:20 +0800

    '删除env文件'

--- run.py
+++ run.py
@@ -3,21 +3,17 @@ from httprunner.report.html import (gen_html_report)
 from tools import DingTalk
 from datetime import datetime
 
-enviroment = "test"
-
 if __name__ == '__main__':
     # runner = HttpRunner(failfast=False, log_level="debug")
-
     runner = HttpRunner(failfast=False, log_level="debug")
-
     # result = runner.run("testcase/hospital/HospitalizedRegister.yml", dot_env_path="test.env")
     # result = runner.run("testcase/hospital/queryHospitalizedRegister.yml", dot_env_path="test.env")
-    result = runner.run("api/token.yml", dot_env_path=".env" if not enviroment else enviroment+".env")
+    result = runner.run("api/token.yml", dot_env_path=".env")
 
     now_timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
     # 生成报告
     gen_html_report(result, now_timestamp)
-
+    # 钉钉通知
     DingTalk.DingTalkSend(now_timestamp, result)
 
 
